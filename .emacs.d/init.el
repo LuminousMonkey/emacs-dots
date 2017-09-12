3089127548d056b50f6dece84306ed5150251b6b
Mike Aldred <mike.aldred@luminousmonkey.org>
2017-09-12 20:24:18 +0800

    Updates for C++ and clang

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -68,3 +68,26 @@
 ;; annoying.
 (setq vc-follow-symlinks t)
 (org-babel-load-file (expand-file-name "monkey.org" user-emacs-directory))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   (quote
+    ((eval add-to-list
+           (quote auto-mode-alist)
+           (quote
+            ("\\.h\\'" . c++-mode)))
+     (c-mode
+      (indent-tabs-mode))
+     (c++-mode
+      (indent-tabs-mode))
+     (nil
+      (flycheck-clang-language-standard . "c++11")
+      (flycheck-clang-include-path "." "src")
+      (eval add-to-list
+            (quote auto-mode-alist)
+            (quote
+             ("\\.h\\'" . c++-mode))))))))
