ec8a30ee42158bfa3e94096b13bef61d87cf0e48
Mike Aldred <mike.aldred@luminousmonkey.org>
2019-06-20 21:27:50 +0800

    Get Yasnippet working globally.

--- .emacs.d/programming/programming-yasnippet.el
+++ .emacs.d/programming/programming-yasnippet.el
@@ -1,11 +1,13 @@
 ;; -*- coding: utf-8; lexical-binding: t; -*-
 
 (use-package yasnippet
+  :init
+  (yas-global-mode t)
   :config
   (use-package yasnippet-snippets)
   ;; Adding yasnippet support to company
-  (add-to-list 'company-backends '(company-yasnippet))
-  (yas-global-mode t)
+  (with-eval-after-load "company"
+    (add-to-list 'company-backends '(company-yasnippet)))
   :diminish yas-minor-mode)
 
 (provide 'programming-yasnippet)
